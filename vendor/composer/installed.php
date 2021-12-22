74511218bbef83658f283e3a657cb4cea6623648
Mike Hemberger <mike@thestizmedia.com>
2021-12-21 19:43:44 -0500

    make sure wpdi is loaded from engine

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -6,29 +6,17 @@
     'aliases' => 
     array (
     ),
-    'reference' => '7608f50225f2956a1243e4e59e5c0e8c3155011b',
+    'reference' => 'a6aaf5ddb88faeeab7dd2f7bc337aa77078313b0',
     'name' => 'maithemewp/mai-delight',
   ),
   'versions' => 
   array (
     'afragen/wp-dependency-installer' => 
     array (
-      'pretty_version' => '4.2.2',
-      'version' => '4.2.2.0',
-      'aliases' => 
+      'replaced' => 
       array (
+        0 => '*',
       ),
-      'reference' => '5e5419348c6cde7b02f3c33fb3fc3381062443a1',
-    ),
-    'afragen/wp-dismiss-notice' => 
-    array (
-      'pretty_version' => 'dev-main',
-      'version' => 'dev-main',
-      'aliases' => 
-      array (
-        0 => '9999999-dev',
-      ),
-      'reference' => '21949732f1ea21d81500bdda8f882ac7eb0f98af',
     ),
     'maithemewp/mai-delight' => 
     array (
@@ -37,7 +25,7 @@
       'aliases' => 
       array (
       ),
-      'reference' => '7608f50225f2956a1243e4e59e5c0e8c3155011b',
+      'reference' => 'a6aaf5ddb88faeeab7dd2f7bc337aa77078313b0',
     ),
     'maithemewp/mai-installer' => 
     array (
@@ -47,7 +35,7 @@
       array (
         0 => '9999999-dev',
       ),
-      'reference' => '244cf7604a6b34a4662c29385497d4257f175956',
+      'reference' => '1b637076d114af7762855493b9bac009a6272d5c',
     ),
   ),
 );
